35ac1b20dccda4fdbcc13648e80a0a3fb8d8ef97
mehruddin11 <mehruddinkhan@mehruddins-MacBook-Air.local>
2023-09-23 00:05:46 +0530

    daily challanges

---
+++ 0LeetcodedailyChallanges/issubsequence.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        # mine
+        count = 0
+        for i in range(len(t)):
+            if (count < len(s) and s[count] == t[i]):
+                count+=1
+        return count== len(s)
+
